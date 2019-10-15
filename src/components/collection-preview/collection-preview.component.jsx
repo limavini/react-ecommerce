f63c8050b2d3df3412cd0db3aa00c699f509edc4
limavini <vinicius.lalmeida98@gmail.com>
2019-10-15 19:40:09 -0300

    Created cart-item and connected cart-dropdown with reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,9 +7,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, i) => i < 4)
-        .map(({id, ...itemProps}) => (
-          <CollectionItem key={id} {...itemProps} />
+        .filter((_item, i) => i < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
