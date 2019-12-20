d1bea1887376154dbf4862d914a27d7ae2c32cf1
limavini <viniciusalmeidait@gmail.com>
2019-12-19 21:00:16 -0300

    Created cart action, reducer and types. Added onclick function to close button on checkout page.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
+
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({cartItem: { name, imageUrl, quantity, price }}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+const CheckoutItem = ({ cartItem, clearItemFromCart }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
 
-export default CheckoutItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>&#10005;</div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  clearItemFromCart: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import { CartActionTypes } from './cart.types';
-import { addItemToCart } from  "./cart.utils";
+import { CartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
@@ -13,7 +13,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
     case CartActionTypes.ADD_ITEM:
-      return {...state, cartItems: addItemToCart(state.cartItems, action.payload)}
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
