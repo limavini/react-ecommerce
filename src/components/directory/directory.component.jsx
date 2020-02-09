466b5d3bb2062a470296bba9388b7d1340769515
limavini <viniciusalmeidait@gmail.com>
2020-02-08 23:42:43 -0300

    Passing directory sections to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import MenuItem from "../menu-item/menu-item.component";
-import SECTIONS from "./directory.data";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import "./directory.styles.scss";
 
-const Directory = () => {
-  const sections = SECTIONS;
+const Directory = ({sections}) => {
 
-  return (
-    <div className="directory-menu">
-      {sections.map(({ id, ...otherSectionProps }) => (
-        <MenuItem key={id} {...otherSectionProps} />
-      ))}
-    </div>
-  );
-};
+  return <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>;
+}
 
-export default Directory;
+  const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+  })
+
+export default connect(mapStateToProps)(Directory);
