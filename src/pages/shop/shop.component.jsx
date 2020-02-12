8ea36b2f43e744ecfc430c8cd99cb591f9cacf14
limavini <viniciusalmeidait@gmail.com>
2020-02-11 21:05:59 -0300

    Added shop to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import {selectCollections} from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import SHOP_DATA from "./shop.data";
 
-const ShopPage = () => {
-  const collections = SHOP_DATA;
-
-  return (
+const ShopPage = ({collections}) =>  (
     <div className="shop-page">
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
     </div>
   );
-};
 
-export default ShopPage;
+  const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+  })
+
+export default connect(mapStateToProps)(ShopPage);
